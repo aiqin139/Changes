a8cb4714867eae276324fa000a874ed629194a06
aicodeone <277579066@qq.com>
2021-02-01 14:24:37 +0800

    Prediction：增加数字卦逻辑

--- Change/Models/Prediction.swift
+++ Change/Models/Prediction.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+var val1Str: String = "0"
+var val2Str: String = "0"
+var val3Str: String = "0"
+
+struct Prediction {
+    var hexagrams: [Hexagram]
+    
+    public struct Result {
+        var hexagram: Hexagram
+        var explanation: [String]
+    }
+    
+    public func DigitPrediction(value1: Int, value2: Int, value3: Int) ->Result {
+        let part1 = (value1 % 8) != 0 ? (value1 % 8) : 8
+        let part2 = (value2 % 8) != 0 ? (value2 % 8) : 8
+        let index = (value3 % 6) != 0 ? (value3 % 6) : 6
+        
+        let hexagram: Hexagram = hexagrams.filter { $0.id == (8 * (part1 - 1)) + part2 }[0]
+        
+        return Result(hexagram: hexagram, explanation: hexagram.explanations[index])
+    }
+}
