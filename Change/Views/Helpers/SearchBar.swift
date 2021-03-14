c44d226aeaafc090b784d85c241243eee2ec349b
aicodeone <277579066@qq.com>
2021-03-14 21:12:27 +0800

    Helpers：增加SearchBar控件

---
+++ Change/Views/Helpers/SearchBar.swift
@@ -0,0 +1,32 @@
+//
+//  SearchBar.swift
+//  Change
+//
+//  Created by aiqin139 on 2021/3/14.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    
+    var body: some View {
+        TextField("Search ...", text: $text)
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .overlay(
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 8)
+            )
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant(""))
+    }
+}
