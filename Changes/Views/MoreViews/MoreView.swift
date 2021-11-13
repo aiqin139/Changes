b3a95a29b2203d180f92368a16784e8501e5f9ab
aicodeone <277579066@qq.com>
2021-11-13 18:58:24 +0800

    MoreView：修改RTFView为RTFReader

--- Changes/Views/MoreViews/MoreView.swift
+++ Changes/Views/MoreViews/MoreView.swift
@@ -22,53 +22,53 @@ struct MoreNavigationView: View {
             }
             
             Section {
-                NavigationLink(destination:  RTFView(fileName: "占卦须知").navigationTitle("占卦须知")) {
+                NavigationLink(destination:  RTFReader(fileName: "占卦须知").navigationTitle("占卦须知")) {
                     Text("占卦须知")
                 }
 
-                NavigationLink(destination:  RTFView(fileName: "大衍占法").navigationTitle("大衍占法")) {
+                NavigationLink(destination:  RTFReader(fileName: "大衍占法").navigationTitle("大衍占法")) {
                     Text("大衍占法")
                 }
                 
-                NavigationLink(destination:  RTFView(fileName: "数字占法").navigationTitle("数字占法")) {
+                NavigationLink(destination:  RTFReader(fileName: "数字占法").navigationTitle("数字占法")) {
                     Text("数字占法")
                 }
 
-                NavigationLink(destination:  RTFView(fileName: "念念有词").navigationTitle("念念有词")) {
+                NavigationLink(destination:  RTFReader(fileName: "念念有词").navigationTitle("念念有词")) {
                     Text("念念有词")
                 }
             }
             
             Section {
-                NavigationLink(destination:  RTFView(fileName: "占卦三不").navigationTitle("占卦三不")) {
+                NavigationLink(destination:  RTFReader(fileName: "占卦三不").navigationTitle("占卦三不")) {
                     Text("占卦三不")
                 }
                 
-                NavigationLink(destination:  RTFView(fileName: "不诚不占").navigationTitle("不诚不占")) {
+                NavigationLink(destination:  RTFReader(fileName: "不诚不占").navigationTitle("不诚不占")) {
                     Text("不诚不占")
                 }
                 
-                NavigationLink(destination:  RTFView(fileName: "不义不占").navigationTitle("占卦三不")) {
+                NavigationLink(destination:  RTFReader(fileName: "不义不占").navigationTitle("占卦三不")) {
                     Text("不义不占")
                 }
                 
-                NavigationLink(destination:  RTFView(fileName: "不疑不占").navigationTitle("不疑不占")) {
+                NavigationLink(destination:  RTFReader(fileName: "不疑不占").navigationTitle("不疑不占")) {
                     Text("不疑不占")
                 }
             }
             
             Section {
-                NavigationLink(destination:  RTFView(fileName: "元亨之意").navigationTitle("元亨之意")) {
+                NavigationLink(destination:  RTFReader(fileName: "元亨之意").navigationTitle("元亨之意")) {
                     Text("元亨之意")
                 }
                 
-                NavigationLink(destination:  RTFView(fileName: "利贞之意").navigationTitle("利贞之意")) {
+                NavigationLink(destination:  RTFReader(fileName: "利贞之意").navigationTitle("利贞之意")) {
                     Text("利贞之意")
                 }
             }
             
             Section {
-                NavigationLink(destination:  RTFView(fileName: "软件许可").navigationTitle("关于软件")) {
+                NavigationLink(destination:  RTFReader(fileName: "软件许可").navigationTitle("关于软件")) {
                     Text("关于软件")
                 }
             }
