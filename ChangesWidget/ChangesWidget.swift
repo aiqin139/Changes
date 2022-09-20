de6a24458a5c55c9abbd20c17af7d9a5cc5e4e2f
aicodeone <277579066@qq.com>
2022-09-20 19:40:51 +0800

    Changes：初步增加小组件（空壳），并去除LocationProvider，以及获取地理位置提示

--- ChangesWidget/ChangesWidget.swift
+++ ChangesWidget/ChangesWidget.swift
@@ -2,30 +2,31 @@
 //  ChangesWidget.swift
 //  ChangesWidget
 //
-//  Created by aiqin139 on 2022/6/10.
+//  Created by aiqin139 on 2022/8/9.
 //
 
 import WidgetKit
 import SwiftUI
+import Intents
 
-struct Provider: TimelineProvider {
+struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
 
@@ -36,32 +37,112 @@ struct Provider: TimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+    let configuration: ConfigurationIntent
+}
+
+struct SmallView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                DerivedSymbol(id: 20)
+                    .frame(width: 50, height: 50, alignment: .center)
+                
+                VStack {
+                    Text("时运")
+                    Text("乾卦")
+                }
+            }
+            Text("品行端正，名利自成。")
+        }
+    }
+}
+
+struct MediumView: View {
+
+    var body: some View {
+        HStack {
+            
+            let digitalData = DigitalData()
+            
+            VStack {
+                DerivedSymbol(id: digitalData.id)
+                    .frame(width: 100, height: 100, alignment: .center)
+            }
+            VStack {
+                Text("初")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+        }
+        .padding()
+    }
+}
+
+struct LargeView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                DerivedSymbol(id: 20)
+                    .frame(width: 100, height: 100, alignment: .center)
+                Text("哈哈")
+            }
+            
+            VStack {
+                Text("测试")
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+        }
+        .padding()
+    }
 }
 
 struct ChangesWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    @ViewBuilder
     var body: some View {
-        Text(entry.date, style: .time)
+        VStack {
+            switch widgetFamily {
+            case .systemSmall:
+                SmallView()
+            case .systemMedium:
+                MediumView()
+            case .systemLarge:
+                LargeView()
+            default:
+                Text(entry.date, style: .time)
+            }
+        }
     }
 }
 
 @main
 struct ChangesWidget: Widget {
-    let kind: String = "ChangesWidget"
+    let kind: String = "易经"
+    
+    @Environment(\.widgetFamily) var widgetFamily
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+        IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             ChangesWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("占卦设置")
+        .description("逢凶化吉，遇难呈祥")
     }
 }
 
 struct ChangesWidget_Previews: PreviewProvider {
     static var previews: some View {
-        ChangesWidgetEntryView(entry: SimpleEntry(date: Date()))
+        ChangesWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+        
+        ChangesWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemMedium))
+        
+        ChangesWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            .previewContext(WidgetPreviewContext(family: .systemLarge))
     }
 }
